000e3c743644dc7dbd956db03f787035980077fe
Zach <zowen17@gmail.com>
2026-08-04 20:43:14 -0400

    added cleaner code and updated pipeline code to reflect that

--- src/pipeline.ts
+++ src/pipeline.ts
@@ -1,9 +1,12 @@
 import { scrapeBooks } from './scraper';
+import { cleanBooks } from './cleaner';
 
 const run = async () => {
     const rawBooks = await scrapeBooks(1);
-    console.log('First book raw data');
-    console.log(rawBooks[0]);
+    const cleanedBooks = cleanBooks(rawBooks);
+
+    console.log('First book cleaned:');
+    console.log(cleanedBooks[0]);
 }
 
 run();
